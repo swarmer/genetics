01044bda50c75449f2192c0d01c6e7c51f6bc1a2
Anton Barkovsky <anton@swarmer.me>
2017-04-30 01:39:07 +0300

    CORS

--- genetics_api/app.py
+++ genetics_api/app.py
@@ -3,7 +3,18 @@ import falcon
 from .resources import BioticFactorResource, TaxonResource
 
 
-app = application = falcon.API()
+class CorsMiddleware(object):
+    def process_request(self, req, resp):
+        pass
+
+    def process_resource(self, req, resp, resource, params):
+        pass
+
+    def process_response(self, req, resp, resource, req_succeeded):
+        resp.set_header('Access-Control-Allow-Origin', '*')
+
+
+app = application = falcon.API(middleware=CorsMiddleware())
 
 app.add_route('/taxon', TaxonResource())
 app.add_route('/biotic_factor', BioticFactorResource())
