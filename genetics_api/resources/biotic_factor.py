d85f377d1f629b15a6a7f5f57e2c86d2771870dd
Anton Barkovsky <anton@swarmer.me>
2017-04-29 23:58:06 +0300

    Add POSTS of biotic factors

--- genetics_api/resources/biotic_factor.py
+++ genetics_api/resources/biotic_factor.py
@@ -1,3 +1,5 @@
+import json
+
 import falcon
 import psycopg2.extras
 
@@ -7,16 +9,28 @@ from genetics_api.db import connect
 
 class BioticFactorResource(object):
     def on_get(self, req, resp):
-        connection = connect()
-        cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        with connect() as connection, connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+            cursor.execute('''
+                SELECT id, name, marker_name, taxon1_id, taxon2_id
+                FROM biotic_factors;
+            ''')
+            result = [self._serialize_row(row) for row in cursor.fetchall()]
 
-        cursor.execute('''
-            SELECT id, name, marker_name, taxon1_id, taxon2_id
-            FROM biotic_factors;
-        ''')
-        result = [self._serialize_row(row) for row in cursor.fetchall()]
+            set_json_response(resp, result)
 
-        set_json_response(resp, result)
+    def on_post(self, req, resp):
+        payload = json.load(req.stream)
+
+        with connect() as connection, connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+            cursor.execute(
+                '''
+                    INSERT INTO biotic_factors (name, marker_name, taxon1_id, taxon2_id) VALUES
+                    (%(name)s, %(marker_name)s, %(taxon1_id)s, %(taxon2_id)s)
+                ''',
+                payload,
+            )
+            set_json_response(resp, {})
+
 
     def _serialize_row(self, row):
         return dict(row)
--- genetics_api/resources/taxon.py
+++ genetics_api/resources/taxon.py
@@ -7,26 +7,24 @@ from genetics_api.db import connect
 
 class TaxonResource(object):
     def on_get(self, req, resp):
-        connection = connect()
-        cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        with connect() as connection, connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+            cursor.execute('''
+                SELECT id, english_name, latin_name, thumbnail_url
+                FROM taxons;
+            ''')
+            result = {row['id']: self._serialize_row(row) for row in cursor.fetchall()}
+            for taxon_data in result.values():
+                taxon_data['observations'] = []
 
-        cursor.execute('''
-            SELECT id, english_name, latin_name, thumbnail_url
-            FROM taxons;
-        ''')
-        result = {row['id']: self._serialize_row(row) for row in cursor.fetchall()}
-        for taxon_data in result.values():
-            taxon_data['observations'] = []
+            observations = self.get_observations(cursor, set(result.keys()))
+            for observation in observations:
+                taxon_data = result[observation['taxon_id']]
+                taxon_data['observations'].append({
+                    'latitude': observation['latitude'],
+                    'longitude': observation['longitude'],
+                })
 
-        observations = self.get_observations(cursor, set(result.keys()))
-        for observation in observations:
-            taxon_data = result[observation['taxon_id']]
-            taxon_data['observations'].append({
-                'latitude': observation['latitude'],
-                'longitude': observation['longitude'],
-            })
-
-        set_json_response(resp, result)
+            set_json_response(resp, result)
 
     def get_observations(self, cursor, taxon_ids):
         cursor.execute(
