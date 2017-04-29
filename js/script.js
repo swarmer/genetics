68dc8eecf9d4cb66a7771693cf773bbea1b955a2
Max Masarnovsky <m.masarnovsky@gmail.com>
2017-04-30 02:23:50 +0300

    output data in ul

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 function search(val, spiece) {
+
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://genetics-api.swarmer.me/taxon?search=' + val, true);
 
@@ -7,27 +8,20 @@ function search(val, spiece) {
     xhr.onload = function() {
        text = this.responseText;
        jsn = JSON.parse(text);
-       console.log(jsn[0]['english_name']);
-       console.log(jsn.length);
+       //console.log(jsn[0]['english_name']);
+       //console.log(jsn.length);
 
+        document.getElementById('sp').innerHTML = '';
+       var inn = '<ul>';
        for (var i=0; i<jsn.length; i++){
-           document.getElementById(spiece);
+           inn += '<li>' + jsn[i]['english_name'] + '</li>';
        }
+       inn += '</ul>';
+        document.getElementById('sp').innerHTML = inn;
     }
-
-    //console.log(text);
-    //console.log(JSON.parse(text));
 
     xhr.onerror = function() {
         alert( 'Ошибка ' + this.status );
     }
     xhr.send();
-
-
-    // var x = new XMLHttpRequest();
-    // x.open("GET", "http://genetics-api.swarmer.me/taxon?search=" + val, true);
-    // x.onload = function (){
-    //     alert( x.responseText);
-    // }
-    // x.send(null);
 }
