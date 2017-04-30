fb842e32aa202ae19a59cb6249ec5b092aaf6434
Max Masarnovsky <m.masarnovsky@gmail.com>
2017-04-30 03:19:00 +0300

    frontend search final

--- js/script.js
+++ js/script.js
@@ -1,27 +1,47 @@
 function search(val, spiece) {
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://genetics-api.swarmer.me/taxon?search=' + val, true);
-
-    var text;
-    var jsn;
-    xhr.onload = function() {
-       text = this.responseText;
-       jsn = JSON.parse(text);
-       //console.log(jsn[0]['english_name']);
-       //console.log(jsn.length);
-
-        document.getElementById('sp').innerHTML = '';
-       var inn = '<ul>';
-       for (var i=0; i<jsn.length; i++){
-           inn += '<li>' + jsn[i]['english_name'] + '</li>';
-       }
-       inn += '</ul>';
-        document.getElementById('sp').innerHTML = inn;
+    if (val == ''){
+        document.getElementById('ul'+spiece).innerHTML = '';
     }
+    else {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://genetics-api.swarmer.me/taxon?search=' + val, true);
 
-    xhr.onerror = function() {
-        alert( 'Ошибка ' + this.status );
+        var text;
+        var jsn;
+        xhr.onload = function() {
+            text = this.responseText;
+            jsn = JSON.parse(text);
+            //console.log(jsn[0]['english_name']);
+            //console.log(jsn.length);
+
+            document.getElementById('ul'+spiece).setAttribute('class', 'collection');
+            document.getElementById('ul'+spiece).innerHTML = '';
+            //var inn = '<ul>';
+            var inn = ''
+            var size = jsn.length;
+            if (size > 6)
+                size = 6;
+            for (var i=0; i<size; i++){
+                //inn += '<li>' + jsn[i]['english_name'] + '</li>';
+                inn += '<li class="collection-item searchItem" onclick="selectItem(this,' + spiece + ')">' + jsn[i]['english_name'] + '</li>';
+            }
+            document.getElementById('ul'+spiece).innerHTML = inn;
+        }
+
+        xhr.onerror = function() {
+            alert( 'Ошибка ' + this.status );
+        }
+        xhr.send();
     }
-    xhr.send();
+}
+
+function clearUl(spiece) {
+    // document.getElementById(spiece).value = '';
+    // document.getElementById('ul'+spiece).innerHTML = '';
+    // document.getElementById('ul'+spiece).setAttribute('class', '');
+}
+
+function selectItem(item, spiece) {
+    console.log(item);
+    document.getElementById(spiece.id).value = item.value;
 }
