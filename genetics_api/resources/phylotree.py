b92de511c56080e25216c765510da9db10f1e91d
Anton Barkovsky <anton@swarmer.me>
2017-05-04 21:31:26 +0300

    Update phylotree

--- genetics_api/resources/phylotree.py
+++ genetics_api/resources/phylotree.py
@@ -1,3 +1,7 @@
+import contextlib
+import os
+import random
+import string
 import subprocess
 import tempfile
 
@@ -13,19 +17,31 @@ def unfuck_clustal_name(name):
     return name.replace('_', ' ')
 
 
+@contextlib.contextmanager
+def tempname():
+    name = '/tmp/' + ''.join(random.choise(string.ascii_letters) for _ in range(20))
+
+    yield name
+
+    try:
+        os.remove(name)
+    except FileNotFoundError:
+        pass
+
+
 def get_tree_bytes(pairs):
     with tempfile.NamedTemporaryFile(mode='w') as clustal_input, \
-            tempfile.NamedTemporaryFile(mode='r') as phylogeny_input:
+            tempname() as phylogeny_input:
         for name, sequence in pairs:
             clustal_input.write('> %s\n%s\n\n' % (encode_for_clustal(name), sequence))
         clustal_input.flush()
 
         subprocess.check_call([
-            '/clustalo', '-i', clustal_input.name, '--outfmt=clu', '-o', phylogeny_input.name
+            '/clustalo', '-i', clustal_input.name, '--outfmt=clu', '-o', phylogeny_input
         ])
 
         subprocess.check_call([
-            'cat', phylogeny_input.name
+            'cat', phylogeny_input
         ])
 
 
