272c188c98fb751a6245d726944bda43cf439281
Anton Barkovsky <anton@swarmer.me>
2017-05-04 22:31:35 +0300

    Update phylotree

--- genetics_api/resources/phylotree.py
+++ genetics_api/resources/phylotree.py
@@ -1,9 +1,11 @@
 import contextlib
 import os
 import random
+import requests
 import string
 import subprocess
 import tempfile
+import time
 
 from Bio import Phylo
 import pylab
@@ -29,6 +31,41 @@ def tempname():
         pass
 
 
+def get_tree_desc(phylogeny_input):
+    with open(phylogeny_input) as infile:
+        data = infile.read()
+
+        response = requests.post(
+            'http://www.ebi.ac.uk/Tools/services/rest/simple_phylogeny/run/',
+            headers={
+                'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+            },
+            params={
+                'email': 'anton@swarmer.me',
+                'sequence': data,
+            }
+        )
+        assert response.status_code == 200
+        job_id = response.text
+
+        while True:
+            response = requests.get(
+                'http://www.ebi.ac.uk/Tools/services/rest/simple_phylogeny/status/%s' % job_id,
+            )
+            if response.text == 'FINISHED':
+                break
+
+            time.sleep(1)
+
+        response = requests.get(
+            'http://www.ebi.ac.uk/Tools/services/rest/simple_phylogeny/result/%s/tree' % job_id,
+        )
+        assert response.status == 200
+        tree_desc = response.text
+
+        return tree_desc
+
+
 def get_tree_bytes(pairs):
     with tempfile.NamedTemporaryFile(mode='w') as clustal_input, \
             tempname() as phylogeny_input:
@@ -40,9 +77,8 @@ def get_tree_bytes(pairs):
             '/clustalo', '-i', clustal_input.name, '--outfmt=clu', '-o', phylogeny_input
         ])
 
-        subprocess.check_call([
-            'cat', phylogeny_input
-        ])
+        tree_desc = get_tree_desc(phylogeny_input)
+        print(tree_desc)
 
 
 def draw_tree(input_path, output_path):
